63f8d34daba345abf59ea73d52c14132628e08b2
MANIRAMAYASAPPA <119868607+MANIRAMAYASAPPA@users.noreply.github.com>
2023-05-31 21:26:24 +0530

    #include<stdio.h> int main() {     int n,i,sum=0,m;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(n%i==0)         {             sum++;         }     }     if(sum==2)     {         printf("prime");     }     else     {         printf("not a prime");     } }

--- prime_number.c
+++ prime_number.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,fc=0;
+    int n,i,sum=0,m;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
         if(n%i==0)
         {
-            fc++;
+            sum++;
         }
     }
-    if(fc==2)
+    if(sum==2)
     {
         printf("prime");
     }
